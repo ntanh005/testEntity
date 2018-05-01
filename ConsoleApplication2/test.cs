8867d4ec08654bbdfeadb8fc6f3ba40fc5e9cd08
Anh Nguyen <ntanh005@ggmail.com>
2018-05-01 22:21:53 +0700

    bong.dom

--- ConsoleApplication2/test.cs
+++ ConsoleApplication2/test.cs
@@ -6,11 +6,7 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication2
 {
-    class Program
+    class test
     {
-        static void Main(string[] args)
-        {
-            ff
-        }
     }
 }
